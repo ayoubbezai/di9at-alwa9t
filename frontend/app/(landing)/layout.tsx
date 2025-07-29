5f34fe2f4bea9c9dc89f6bf35dc595dc95124fd1
ayoubbezai <bezaiayoub8@gmail.com>
2025-07-29 04:30:53 +0100

    fix width of layout

--- frontend/app/(landing)/layout.tsx
+++ frontend/app/(landing)/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NaNvBar />
-        <div className="container mx-auto">{children}</div>
+        <div className="container max-w-7xl w-full  mx-auto">{children}</div>
       </body>
     </html>
   );
